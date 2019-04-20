c7b80a7fd12fe154d696bbce3f2fdf492a501cdf
Twonki <Leonhard.Applis@Protonmail.com>
2019-04-20 17:35:37 +0200

    Cleaning Game Draft and small changes

--- Game.hs
+++ Game.hs
@@ -13,33 +13,22 @@ type Move = (Pos,Pos)
 changePlayer' :: GameState -> GameState
 changePlayer' = (\(b,p)->(b, changePlayer p))
 
-movePiece :: Pos -> Pos -> GameState -> GameState
-movePiece s e g@(b,c) = 
+movePiece :: GameState -> Move ->  GameState
+movePiece g@(b,c) (s,e) = 
         if canPickUp s g
-        then 
-            case f of
-                (Just f') -> 
-                    if canMove f' e g 
-                    then 
+        then case f of
+                (Just f') ->
                         let  m = moveTo b f' e
-                        in 
-                            case m of 
-                                Just m' -> (m',changePlayer c) 
-                                otherwise -> g
-                    else g 
+                        in case m of 
+                            Just m' -> (m',changePlayer c) 
+                            otherwise -> g
+                otherwise -> g 
         else g -- Someone Tried to do something invalid - nothing happens
     where f = pieceOnPos b s
 
 canPickUp:: Pos -> GameState -> Bool 
-canPickUp p (b,c) = demaybiebool maybeSameColour
+canPickUp p (b,c) = demaybiebool $ fmap (((==) c) . player) f
     where f = pieceOnPos b p
-          maybeSameColour = fmap (((==) c) . player) f 
-
-canMove:: Chesspiece -> Pos -> GameState -> Bool 
-canMove f p g@(b,c)
-        | trymove == Nothing = False 
-        | otherwise = True 
-    where trymove = moveTo b f p
 
 demaybiebool :: Maybe Bool -> Bool 
 demaybiebool Nothing = False 
