7cc56449a4a566081fc1bd7c82c2a1e88cd7b1fa
Twonki <Leonhard.Applis@Protonmail.com>
2019-04-28 10:00:37 +0200

    Restructured and Basic Cabal running #28

--- Src/Game.hs
+++ Src/Game.hs
@@ -8,27 +8,15 @@ type Move = (Pos,Pos)
 
 main :: IO () 
 main = do
+    let g@(b,c) = initialGameState
     putStrLn "Hello, lets start Chess!"
-    gameLoop initialGameState
+    inp <- getLine 
+    let s@(x,y) = read (inp) :: (Int,Int)
+    putStrLn (show s)
 
-gameLoop :: GameState -> IO ()
+gameLoop :: GameState -> IO GameState
 gameLoop oldS@(b,c) = do 
-    if (lost oldS c)
-    then do putStrLn ( show c ++ " lost")
-    else do
-        putStrLn (printBoard b)
-        putStrLn ("Its " ++ show c ++ "s turn")
-        move <- readMove
-        let newS = movePiece oldS move
-        if (newS == oldS) 
-        then 
-            do
-                putStrLn "Invalid move! Try Again"
-                gameLoop oldS
-        else gameLoop newS
-            
-readMove :: IO Move
-readMove = do 
+    putStrLn ("Its " ++ show c ++ " turn")
     putStrLn "Which piece to move?"
     inp <- getLine 
     let s = read (inp) :: (Int,Int)
@@ -36,7 +24,15 @@ readMove = do
     inp2 <- getLine 
     let e = read (inp2) :: (Int,Int)
     putStrLn ("Move " ++ show s ++ " to " ++ show e)
-    return (s,e)
+    let newS = movePiece oldS (s,e)
+    if (newS == oldS) 
+        then 
+            do
+                putStrLn "Invalid move! Try Again"
+                gameLoop oldS
+        else 
+            gameLoop newS
+            
 
 changePlayer' :: GameState -> GameState
 changePlayer' = (\(b,p)->(b, changePlayer p))
