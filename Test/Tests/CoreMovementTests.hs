9134d5c219f3bfdf011e604b451a7343c3355fa0
Twonki <Leonhard.Applis@Protonmail.com>
2019-04-29 21:46:22 +0200

    Refactored CoreMovementTests for #38

--- Test/Tests/CoreMovementTests.hs
+++ Test/Tests/CoreMovementTests.hs
@@ -3,80 +3,80 @@ module Tests.CoreMovementTests (allCoreMovementTests) where
 import Tests.TestSuite
 
 allCoreMovementTests = TestList [
-    TestLabel "KingCoreMoves" allKingCoreMoves
-    , TestLabel "KnightCoreMOves" allKnightCoreMoves
-    , TestLabel "Lines Function" allLines 
-    , TestLabel "StopAtNearest" allStopAt
-    , TestLabel "OnBoard" allOnBoard
+    TestLabel "KingCoreMoves testStopped" allKingCoreMoves
+    , TestLabel "KnightCoreMoves testStopped" allKnightCoreMoves
+    , TestLabel "Lines Function testStopped " allLines 
+    , TestLabel "StopAtNearest testStopped" allStopAt
+    , TestLabel "OnBoard testStopped" allOnBoard
     ]
 
 allKingCoreMoves = TestList [
-    TestLabel "Free " testKF,
-    TestLabel "Cornered" testKC,
-    TestLabel "Cornered MoveFilter" testKCM
+    TestLabel "Centered " testFreeKing,
+    TestLabel "Cornered" testKingCorneredNoFilter,
+    TestLabel "Cornered MoveFilter" testKingCorneredFiltered
     ]
 
-testKF = 8 ~=? length (kingMoves (5,5))
-testKC = 8 ~=? length (kingMoves (8,8))
-testKCM = 3 ~=? length (moveFilter [Chesspiece King (8,8) W] (kingMoves (8,8)))
+testFreeKing = 8 ~=? length (kingMoves (5,5))
+testKingCorneredNoFilter = 8 ~=? length (kingMoves (8,8))
+testKingCorneredFiltered = 3 ~=? length (moveFilter [Chesspiece King (8,8) W] (kingMoves (8,8)))
 
 allKnightCoreMoves = TestList [
-    TestLabel "Free " testHF,
-    TestLabel "Cornered" testHC,
-    TestLabel "Cornered MoveFilter" testHCM
+    TestLabel "Centered " testFreeKnight,
+    TestLabel "Cornered" testKnightCorneredNotFiltered,
+    TestLabel "Cornered MoveFilter" testKnightCorneredFiltered
     ]
 
-testHF = 8 ~=? length (knightMoves (5,5))
-testHC = 8 ~=? length (knightMoves (8,8))
-testHCM = 2 ~=? length (moveFilter [Chesspiece Knight (8,8) W] (knightMoves (8,8)))
+testFreeKnight = 8 ~=? length (knightMoves (5,5))
+testKnightCorneredNotFiltered = 8 ~=? length (knightMoves (8,8))
+testKnightCorneredFiltered = 2 ~=? length (moveFilter [Chesspiece Knight (8,8) W] (knightMoves (8,8)))
 
 allLines = TestList [
-    TestLabel "Rights Count" testHL1,
-    TestLabel "Lefts Count" testHL2,
-    TestLabel "Ups Count" testVL1,
-    TestLabel "Downs Count" testVL2,
-    TestLabel "Rising Digs R Count" testRL1,
-    TestLabel "Rising Digs L Count" testRL2,
-    TestLabel "Falling Digs R Count" testFL1,
-    TestLabel "Falling Digs L Count" testFL2,
-    TestLabel "Lines do not have themselves in it I" testLN1,
-    TestLabel "Lines do not have themselves in it II" testLN2
+    TestLabel "Rights Count" testRightsCount,
+    TestLabel "Lefts Count" testLeftsCount,
+    TestLabel "Ups Count" testUpsCount,
+    TestLabel "Downs Count" testDownsCount,
+    TestLabel "Rising Digs R Count" testRightRisingDigsCount,
+    TestLabel "Rising Digs L Count" testLeftRisingDigsCount,
+    TestLabel "Falling Digs R Count" testRightFallingDigsCount,
+    TestLabel "Falling Digs L Count" testLeftFallingDigsCount,
+    TestLabel "Lines do not have themselves in it I" testNoVerticalSelfcontainment,
+    TestLabel "Lines do not have themselves in it II" testNoDiagonalSelfcontainment
     ]
 
-testHL1 = 8 ~=? length (rights (5,5))
-testHL2 = 8 ~=? length (lefts (5,5))
-testVL1 = 8 ~=? length (ups (5,5))
-testVL2 = 8 ~=? length (downs (5,5))
-testRL1 = 8 ~=? length (risingDigR (5,5))
-testRL2 = 8 ~=? length (risingDigL (5,5))
-testFL1 = 8 ~=? length (fallingDigR (5,5))
-testFL2 = 8 ~=? length (fallingDigL (5,5))
+testRightsCount = 8 ~=? length (rights (5,5))
+testLeftsCount = 8 ~=? length (lefts (5,5))
+testUpsCount = 8 ~=? length (ups (5,5))
+testDownsCount = 8 ~=? length (downs (5,5))
+testRightRisingDigsCount = 8 ~=? length (risingDigR (5,5))
+testLeftRisingDigsCount = 8 ~=? length (risingDigL (5,5))
+testRightFallingDigsCount = 8 ~=? length (fallingDigR (5,5))
+testLeftFallingDigsCount = 8 ~=? length (fallingDigL (5,5))
 
-testLN1 = False ~=? (5,5) `elem` (ups (5,5))
-testLN2 = False ~=? (5,5) `elem` (fallingDigL (5,5))
+testNoVerticalSelfcontainment = False ~=? (5,5) `elem` (ups (5,5))
+testNoDiagonalSelfcontainment = False ~=? (5,5) `elem` (fallingDigL (5,5))
 
 allStopAt = TestList [
-    TestLabel "Stopped" testS,
-    TestLabel "Not Stopped " testNS,
-    TestLabel "Stopped Late" testSL,
-    TestLabel "Empty Line" testSEL
+    TestLabel "Stopped" testStopped,
+    TestLabel "Not Stopped " testNotStopped,
+    TestLabel "Stopped Late" testStoppedFarAway,
+    TestLabel "Empty Line" testStoppedEmptyLine
     ]
 
 up11 = ups (1,1)
 
-testS = 2 ~=? length (stopAt up11 (1,3))
-testNS = 8 ~=? length (stopAt up11 (2,2))
-testSL = 5 ~=? length (stopAt up11 (1,6))
-testSEL = 0 ~=? length (stopAt [] (1,6))
+testStopped = 2 ~=? length (stopAt up11 (1,3))
+testNotStopped = 8 ~=? length (stopAt up11 (2,2))
+testStoppedFarAway = 5 ~=? length (stopAt up11 (1,6))
+testStoppedEmptyLine = 0 ~=? length (stopAt [] (1,6))
 
 allOnBoard = TestList [
-    TestLabel "X/Y On Board" testXYOB,
-    TestLabel "X not on Board" testXOB,
-    TestLabel "Y not on Board" testYOB,
-    TestLabel "X/Y not on Board" testXYNOB
+    TestLabel "X/Y On Board" testXYOnBoard,
+    TestLabel "X not on Board" testXOnBoardYNot,
+    TestLabel "Y not on Board" testYOnBoardXNot,
+    TestLabel "X/Y not on Board" testYAndYNotOnBoard
     ]
 
-testXYOB = True ~=? onBoard (3,3)
-testXOB = False ~=? onBoard (3,10)
-testYOB = False ~=? onBoard (30,3)
-testXYNOB = False ~=? onBoard (30,10)
+testXYOnBoard = True ~=? onBoard (3,3)
+testXOnBoardYNot = False ~=? onBoard (3,10)
+testYOnBoardXNot = False ~=? onBoard (30,3)
+testYAndYNotOnBoard = False ~=? onBoard (30,10)
