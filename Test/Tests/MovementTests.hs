463730ab2b4e314d2d4e1dbec445a83bd65524d7
Twonki <Leonhard.Applis@Protonmail.com>
2019-04-29 21:09:58 +0200

    Refactored Movement Tests #38

--- Test/Tests/MovementTests.hs
+++ Test/Tests/MovementTests.hs
@@ -3,236 +3,248 @@ module Tests.MovementTests (allMovementTests) where
 import Tests.TestSuite
 
 allMovementTests = TestList [
-    TestLabel "KingMoves" allKingMoves
-    , TestLabel "QueenMoves" allQueenMoves
-    , TestLabel "KnightMoves" allKnightMoves
-    , TestLabel "TowerMoves" allTowerMoves
-    , TestLabel "BishopMoves" allBishopMoves
+    TestLabel "KingMove Tests" allKingMoves
+    , TestLabel "QueenMove Tests" allQueenMoves
+    , TestLabel "KnightMove Tests" allKnightMoves
+    , TestLabel "TowerMoves Tests" allTowerMoves
+    , TestLabel "BishopMove Tests" allBishopMoves
     -- PawnMoves are separate!
  ]
-
 
 -- =======================================
 -- ========== King =======================
 -- =======================================
 allKingMoves = TestList [
-    TestLabel "SafeWhite" testSW,
-    TestLabel "SafeBlack" testSB,
-    TestLabel "MiddleWhite" testMW,
-    TestLabel "MiddleBlack" testMB,
-    TestLabel "Blocked by Friend" testKiBF,
-    TestLabel "Blocked by Enemy" testKiBE 
+    TestLabel "Corner Moves White King" testCorneredWhiteKingMoves,
+    TestLabel "Corner Moves Black King" testCorneredBlackKingMoves,
+    TestLabel "Moves central White King" testCentralWhiteKingMoves,
+    TestLabel "Moves central Black King" testCentralBlackKingMoves,
+    TestLabel "King is Blocked by Ally" testKingBlockedByAllyMoves,
+    TestLabel "King is Blocked by Enemy" testKingBlockedByEnemyMoves 
     ]
+-- I keep these separate because they are important for other tests
+-- If I misscount here, I will also have false tests somewhere else
+-- When these work, I know that some more complex methods do not fail at the kings movement
+testCorneredWhiteKingMoves = 3 ~=? length ( validMoves safeKings W )
+testCorneredBlackKingMoves = 3 ~=? length ( validMoves safeKings B )
 
-testSW = 3 ~=? length ( validMoves safeKings W )
+corneredBlackKing = addKing [] (1,1) B
+centralWhiteKing = addKing corneredBlackKing (5,5) W 
 
-testSB = 3 ~=? length ( validMoves safeKings B )
+testCentralWhiteKingMoves = 8 ~=? length ( validMoves centralWhiteKing W )
 
-mw = addKing [] (1,1) B
-mw' = addKing mw (5,5) W 
+centralBlackKing = addKing [] (5,5) B
+corneredWhiteKing = addKing centralBlackKing (1,1) W 
 
-testMW = 8 ~=? length ( validMoves mw' W )
+testCentralBlackKingMoves = 8 ~=? length ( validMoves corneredWhiteKing B)
 
-mb = addKing [] (5,5) B
-mb' = addKing mb (1,1) W 
-
-testMB = 8 ~=? length ( validMoves mb' B)
-
-kibf = addPawn mw' (6,6) W 
+kingBlockedByFriend = addPawn centralWhiteKing (6,6) W 
 -- 7 Moves for King 
 -- 1 Move for Pawn
-testKiBF = 8 ~=? length ( validMoves kibf W)
+testKingBlockedByAllyMoves = 8 ~=? length ( validMoves kingBlockedByFriend W)
 
-kibe = addPawn mw' (6,6) B 
+kingBlockedByEnemy = addPawn centralWhiteKing (6,6) B 
 -- 7 Moves for King 
 -- 1 Move onto enemy Pawn
-testKiBE = 8 ~=? length ( validMoves kibe W)
+testKingBlockedByEnemyMoves = 8 ~=? length ( validMoves kingBlockedByEnemy W)
 
 -- =======================================
 -- ========== Queen ======================
 -- =======================================
 allQueenMoves = TestList [
-    TestLabel "All Free" testQAF
-    ,TestLabel "Blocked by Friend" testQBBF
-    ,TestLabel "Blocked by Straight Close Enemy" testQBBE
-    ,TestLabel "Blocked by Straight Far Enemy" testQBBFE
-    ,TestLabel "Blocked by Straight Far Far Enemy" testQBBFFE
-    ,TestLabel "Blocked by Dig close Enemy" testQBBDCE
-    ,TestLabel "Blocked by Dig Far Enemy" testQBBDFE
-    ,TestLabel "At Edge of Board" testQAEOB
+    TestLabel "All Free" testQueenAllFreeMoves
+    ,TestLabel "Blocked by Straight Close Friend" testQueenBlockedByCloseFriend
+    ,TestLabel "Blocked by Straight Close Enemy" testQueenBlockedByCloseEnemy
+    ,TestLabel "Blocked by Straight Far Enemy" testQueenBlockedByFarEnemy
+    ,TestLabel "Blocked by Straight Far Far Enemy" testQueenBlockedByFarerEnemy
+    ,TestLabel "Blocked by Dig close Enemy" testQueenBlockedDiagonalByCloseEnemy
+    ,TestLabel "Blocked by Dig far Enemy" testQueenBlockedDiagonalByFarEnemy
+    ,TestLabel "Blocked by Dig close Friend" testQueenBlockedDiagonalByCloseFriend
+    ,TestLabel "Blocked by Dig far Friend" testQueenBlockedDiagonalByFarFriend
+    ,TestLabel "At Edge of Board" testQueenAtTheEdge
  ]
 
-qaf = addQueen safeKings (5,5) W 
+queenAllFree = addQueen safeKings (5,5) W 
 -- 3 Moves for King 
 -- 14 Tower Moves for Queen
 -- 12 Bishop Moves for Queen (On this Position!) 
-testQAF = (3+14+12) ~=? length (validMoves qaf W)
+testQueenAllFreeMoves = (3+14+12) ~=? length (validMoves queenAllFree W)
 
-qbbf = addPawn qaf (5,6) W 
+queenBlockedByCloseFriend = addPawn queenAllFree (5,6) W 
 -- 3 Moves for King
 -- 1 Move for Pawn
 -- 22 Moves for Queen
-testQBBF = (3+1+22) ~=? length (validMoves qbbf W)
+testQueenBlockedByCloseFriend = (3+1+22) ~=? length (validMoves queenBlockedByCloseFriend W)
 
-qbbe = addPawn qaf (5,6) B 
+queenBlockedByCloseEnemy = addPawn queenAllFree (5,6) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- 23 Moves for Queen
-testQBBE = (3+1+23) ~=? length (validMoves qbbe W)
+testQueenBlockedByCloseEnemy = (3+1+23) ~=? length (validMoves queenBlockedByCloseEnemy W)
 
-qbbfe = addPawn qaf (5,7) B 
+queenBlockedByFarEnemy = addPawn queenAllFree (5,7) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- 24 Moves for Queen
-testQBBFE = (3+1+24) ~=? length (validMoves qbbfe W)
+testQueenBlockedByFarEnemy = (3+1+24) ~=? length (validMoves queenBlockedByFarEnemy W)
 -- Pawn is at the Edge, I can fully move
-qbbffe = addPawn qaf (5,8) B 
+queenBlockedByFarerEnemy = addPawn queenAllFree (5,8) B 
 -- 3 Moves for King
 -- 26 Core Queen Moves
-testQBBFFE = (3+26) ~=? length (validMoves qbbffe W)
+testQueenBlockedByFarerEnemy = (3+26) ~=? length (validMoves queenBlockedByFarerEnemy W)
 
 
-qbbdce = addPawn qaf (6,6) B 
+queenBlockedDigByCloseEnemy = addPawn queenAllFree (6,6) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- 23 Moves for Queen
-testQBBDCE = (3+1+23) ~=? length (validMoves qbbdce W)
+testQueenBlockedDiagonalByCloseEnemy = (3+1+23) ~=? length (validMoves queenBlockedDigByCloseEnemy W)
 
-qbbdfe = addPawn qaf (7,7) B 
+queenBlockedDigByFarEnemy = addPawn queenAllFree (7,7) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- 24 Moves for Queen
-testQBBDFE = (3+1+24) ~=? length (validMoves qbbdfe W)
+testQueenBlockedDiagonalByFarEnemy = (3+1+24) ~=? length (validMoves queenBlockedDigByFarEnemy W)
+
+queenBlockedDigByCloseFriend = addPawn queenAllFree (6,6) W 
+-- 3 Moves for King
+-- 23 Moves for Queen
+testQueenBlockedDiagonalByCloseFriend = (3+23) ~=? length (validMoves queenBlockedDigByCloseFriend W)
+
+queenBlockedDigByFarFriend = addPawn queenAllFree (7,7) W 
+-- 3 Moves for King 
+-- 24 Moves for Queen
+testQueenBlockedDiagonalByFarFriend = (3+24) ~=? length (validMoves queenBlockedDigByFarFriend W)
 
 
-qaeob = addQueen safeKings (5,8) W 
+queenAtTheEdge = addQueen safeKings (5,8) W 
 -- 3 Moves for King 
 -- 21 Moves for Queen 
-testQAEOB = 24 ~=? length (validMoves qaeob W)
+testQueenAtTheEdge = 24 ~=? length (validMoves queenAtTheEdge W)
 
 -- =======================================
 -- ========== Knight =====================
 -- =======================================
 allKnightMoves = TestList [
-    TestLabel "All Free" testKAF
-    ,TestLabel "Blocked by Friend" testKBBF
-    ,TestLabel "Blocked by Enemy" testKBBE
-    ,TestLabel "At Edge of Board" testKAEOB
+    TestLabel "All Free" testKnightAllFreeMoves
+    ,TestLabel "Blocked by Friend" testKnightBlockedByFriend
+    ,TestLabel "Blocked by Enemy" testKnightBlockedByEnemy
+    ,TestLabel "At Edge of Board" testKnightAtTheEdge
  ]
 
-kaf = addKnight safeKings (5,5) W 
+knightAllFree = addKnight safeKings (5,5) W 
 -- 3 Moves for King 
 -- 8 Moves for Knight 
-testKAF = (3+8) ~=? length (validMoves kaf W)
+testKnightAllFreeMoves = (3+8) ~=? length (validMoves knightAllFree W)
 
-kbbf = addPawn kaf (6,3) W 
+knightBlockedByFriend = addPawn knightAllFree (6,3) W 
 -- 3 Moves for King 
 -- 1 Move for Pawn 
 -- 7 Moves for Knight
-testKBBF = (3+1+7) ~=? length (validMoves kbbf W)
+testKnightBlockedByFriend = (3+1+7) ~=? length (validMoves knightBlockedByFriend W)
 
-kbbe = addPawn kaf (6,3) B 
+knightBlockedByEnemy = addPawn knightAllFree (6,3) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- 7 Moves for Knight
-testKBBE = (3+1+7) ~=? length (validMoves kbbe W)
+testKnightBlockedByEnemy = (3+1+7) ~=? length (validMoves knightBlockedByEnemy W)
 
-kaeob = addKnight safeKings (5,8) W 
+knightAtTheEdge = addKnight safeKings (5,8) W 
 -- 3 Moves for King 
 -- 4 Moves for Knight 
-testKAEOB = (3+4) ~=? length (validMoves kaeob W)
+testKnightAtTheEdge = (3+4) ~=? length (validMoves knightAtTheEdge W)
     
 -- =======================================
 -- ========== Bishop =====================
 -- =======================================
 allBishopMoves = TestList [
-    TestLabel "All Free" testBAF
-    ,TestLabel "Blocked by Friend" testBBBF
-    ,TestLabel "Blocked by Close Enemy" testBBBCE
-    ,TestLabel "Blocked by Far Enemy" testBBBFE
-    ,TestLabel "At Edge of Board" testBAEOB
+    TestLabel "All Free" testBishopAllFree
+    ,TestLabel "Blocked by Friend" testBishopBlockedByCloseFriend
+    ,TestLabel "Blocked by Close Enemy" testBishopBlockedByCloseEnemy
+    ,TestLabel "Blocked by Far Enemy" testBishopBlockedByFarEnemy
+    ,TestLabel "At Edge of Board" testBishopAtTheEdge
  ]
 
-baf = addBishop safeKings (5,5) W 
+bishopAllFree = addBishop safeKings (5,5) W 
 -- 3 Moves for King 
 -- 12 Moves for Bishop 
-testBAF = 15 ~=? length (validMoves baf W)
+testBishopAllFree = 15 ~=? length (validMoves bishopAllFree W)
 
-bbbf = addPawn baf (5,6) W 
+bishopBlockedByCloseFriend = addPawn bishopAllFree (5,6) W 
 -- 3 Moves for King 
 -- 1 Move for Pawn 
 -- 11 Moves for Bishop
-testBBBF = 15 ~=? length (validMoves bbbf W)
+testBishopBlockedByCloseFriend = 15 ~=? length (validMoves bishopBlockedByCloseFriend W)
 
-bbbce = addPawn baf (5,6) B 
+bishopBlockedByCloseEnemy = addPawn bishopAllFree (5,6) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- 11 Moves for Bishop
-testBBBCE = 15 ~=? length (validMoves bbbce W)
+testBishopBlockedByCloseEnemy = 15 ~=? length (validMoves bishopBlockedByCloseEnemy W)
 
-bbbfe = addPawn baf (6,7) B 
+bishopBlockedByFarEnemy = addPawn bishopAllFree (6,7) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- 12 Moves for Bishop
-testBBBFE = 16 ~=? length (validMoves bbbfe W)
+testBishopBlockedByFarEnemy = 16 ~=? length (validMoves bishopBlockedByFarEnemy W)
 
-baeob = addBishop safeKings (5,8) W 
+bishopAtTheEdge = addBishop safeKings (5,8) W 
 -- 3 Moves for King 
 -- 7 Moves for Bishop 
-testBAEOB = 10 ~=? length (validMoves baeob W)
+testBishopAtTheEdge = 10 ~=? length (validMoves bishopAtTheEdge W)
         
 -- =======================================
 -- ========== Tower ======================
 -- =======================================
 allTowerMoves = TestList [
-    TestLabel "All Free" testTAF
-    ,TestLabel "Blocked by Friend" testTBBF
-    ,TestLabel "Blocked by Close Enemy" testTBBCE
-    ,TestLabel "Blocked by Far Enemy" testTBBFE
-    ,TestLabel "Blocked by Far Enemy Other Direction" testTBBFE2
-    ,TestLabel "Blocked by Far Far Enemy" testTBBFFE
-    ,TestLabel "At Edge of Board" testTAEOB
+    TestLabel "All Free" testTowerAllFree
+    ,TestLabel "Blocked by Close Friend" testTowerBlockedByCloseFriend
+    ,TestLabel "Blocked by Close Enemy" testTowerBlockedByCloseEnemy
+    ,TestLabel "Blocked by Far Enemy" testTowerBlockedByFarEnemy
+    ,TestLabel "Blocked by Far Enemy Other Direction" testTowerBlockedByFarEnemyOnOtherSide
+    ,TestLabel "Blocked by Far Far Enemy" testTowerBlockedByFarerEnemy
+    ,TestLabel "At Edge of Board" testTowerAtTheEdge
  ]
 
-taf = addTower safeKings (5,5) W 
+towerAllFree = addTower safeKings (5,5) W 
 -- 3 Moves for King
 -- A tower can move the whole board of 8 pieces with nothing in the way, but not his own field
 -- Same for Y 
 -- Resulting in (2x7) Moves
 -- X Moves for Tower 
-testTAF = (3+7+7) ~=? length (validMoves taf W)
+testTowerAllFree = (3+7+7) ~=? length (validMoves towerAllFree W)
 
-tbbf = addPawn taf (5,6) W 
+towerBlockedByCloseFriend = addPawn towerAllFree (5,6) W 
 -- 3 Moves for King 
 -- 1 Move for Pawn 
 -- All Tower Moves minus the 4 fields blocked by pawn 
-testTBBF = (3+1+14-4) ~=? length (validMoves tbbf W)
+testTowerBlockedByCloseFriend = (3+1+14-4) ~=? length (validMoves towerBlockedByCloseFriend W)
 
-tbbce = addPawn taf (5,6) B 
+towerBlockedByCloseEnemy = addPawn towerAllFree (5,6) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- All Tower Moves minus the 3 fields blocked by pawn 
-testTBBCE = (3+1+14-3) ~=? length (validMoves tbbce W)
+testTowerBlockedByCloseEnemy = (3+1+14-3) ~=? length (validMoves towerBlockedByCloseEnemy W)
 
-tbbfe = addPawn taf (5,7) B 
+towerBlockedByFarEnemy = addPawn towerAllFree (5,7) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- All Tower Moves minus the 2 fields blocked by pawn 
-testTBBFE = (3+1+14-2) ~=? length (validMoves tbbfe W)
+testTowerBlockedByFarEnemy = (3+1+14-2) ~=? length (validMoves towerBlockedByFarEnemy W)
 
-tbbfe2 = addPawn taf (5,3) B 
+towerBlockedByFarEnemyOtherSide = addPawn towerAllFree (5,3) B 
 -- 3 Moves for King 
 -- 1 Move onto Pawn 
 -- All Tower Moves minus the 2 fields blocked by pawn 
-testTBBFE2 = (3+1+14-2) ~=? length (validMoves tbbfe2 W)
+testTowerBlockedByFarEnemyOnOtherSide = (3+1+14-2) ~=? length (validMoves towerBlockedByFarEnemyOtherSide W)
 
 -- Enemy is on the edge, i can move normal
-tbbffe = addPawn taf (5,8) B 
+towerBlockedByFarerEnemy = addPawn towerAllFree (5,8) B 
 -- 3 Moves for King
 -- 14 base moves for Tower
-testTBBFFE = (3+14) ~=? length (validMoves tbbffe W)
+testTowerBlockedByFarerEnemy = (3+14) ~=? length (validMoves towerBlockedByFarerEnemy W)
 
-taeob = addTower safeKings (5,8) W 
+towerAtTheEdge = addTower safeKings (5,8) W 
 -- 3 Moves for King 
 -- 14 Moves for Tower  
-testTAEOB = (3+7+7) ~=? length (validMoves taeob W)
+testTowerAtTheEdge = (3+7+7) ~=? length (validMoves towerAtTheEdge W)
