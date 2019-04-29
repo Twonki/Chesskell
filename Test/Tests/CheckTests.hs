0dbcf201cac577651782d7ac59b5542569a06309
Twonki <Leonhard.Applis@Protonmail.com>
2019-04-29 21:39:20 +0200

    Refactored Tests for Check #38 and added two more for covered aggressors

--- Test/Tests/CheckTests.hs
+++ Test/Tests/CheckTests.hs
@@ -3,67 +3,76 @@ module Tests.CheckTests (checkTests,checkMateTests,checkLimitedMoveTests) where
 import Tests.TestSuite
 
 checkTests = TestList [
-    TestLabel "No Check White" testNCW
-    ,TestLabel "No Check Black" testNCB
-    ,TestLabel "FarAway Check" testFAC
-    ,TestLabel "Close Check" testCC
-    ,TestLabel "Check Double-Aggressor" testCDA
+    TestLabel "No Initial Check White" testNoInitialCheckWhite
+    ,TestLabel "No Initial Check Black" testNoInitialCheckBlack
+    ,TestLabel "Far Away Aggressor produces Check" testFarAggressorCheck
+    ,TestLabel "Close Aggressor produces Check" testCloseAggressorCheck
+    ,TestLabel "Double-Aggressors produce Check" testDoubleAggressorCheck
     ]
 
 -- Reminder: Safekings means WhiteKing on 1/1 and BlackKing on 8/8
 
-testNCW = False ~=? check safeKings W
-testNCB = False ~=? check safeKings B
+testNoInitialCheckWhite = False ~=? check safeKings W
+testNoInitialCheckBlack = False ~=? check safeKings B
 
-testFAC = True  ~=? check (addTower safeKings (7,1) B) W
-testCC = True  ~=? check (addTower safeKings (2,1) B) W
+testFarAggressorCheck = True  ~=? check (addTower safeKings (7,1) B) W
+testCloseAggressorCheck = True  ~=? check (addTower safeKings (2,1) B) W
 
-cda = addTower (addTower safeKings (2,1) B) (1,7) B
-testCDA = True  ~=? check cda W
+doubleAggressors = addTower (addTower safeKings (2,1) B) (1,7) B
+testDoubleAggressorCheck = True  ~=? check doubleAggressors W
 
 checkMateTests = TestList [
-    TestLabel "No Checkmate White" testNCMW
-    ,TestLabel "No Checkmate Black" testNCMB
-    ,TestLabel "True Checkmate White" testTCMW
-    ,TestLabel "True Checkmate Black" testTCMB
-    ,TestLabel "King can move out of Check" testKMC
-    ,TestLabel "Kill Aggressor" testKA
-    ,TestLabel "ScapeGoat a Piece" testSGP   
+    TestLabel "No Intial Checkmate White" testNoInitialCheckMateWhite
+    ,TestLabel "No Initial Checkmate Black" testNoInitialCheckMateBlack
+    ,TestLabel "White King is in Checkmate" testCheckMateWhite
+    ,TestLabel "White King is in Checkmate" testCheckMateBlack
+    ,TestLabel "King can move out of Check" testCheckMateKingCanMoveOut
+    ,TestLabel "King can kill Aggressor" testCheckMateKingCanRemoveAggressor
+    ,TestLabel "Player can ScapeGoat a Piece" testCheckMateScapegoatOther   
     ]
 
-testNCMW = False ~=? checkmate safeKings W
-testNCMB = False ~=? checkmate safeKings B
+testNoInitialCheckMateWhite = False ~=? checkmate safeKings W
+testNoInitialCheckMateBlack = False ~=? checkmate safeKings B
 
-cmw = addTower (addTower safeKings (1,5) B) (2,5) B
-testTCMW = True ~=? checkmate cmw W
+checkmateWhite = addTower (addTower safeKings (1,5) B) (2,5) B
+testCheckMateWhite = True ~=? checkmate checkmateWhite W
 
-cmb = addTower (addTower safeKings (7,5) W) (8,5) W
-testTCMB = True ~=? checkmate cmb B
+checkmateBlack = addTower (addTower safeKings (7,5) W) (8,5) W
+testCheckMateBlack = True ~=? checkmate checkmateBlack B
 
 -- King can simply move out of check
-testKMC = False ~=? checkmate (addTower safeKings (8,5) W) B 
+testCheckMateKingCanMoveOut = False ~=? checkmate (addTower safeKings (8,5) W) B 
 
 -- Another Unit can kill the Agressor, resolving check
 -- A Black tower can kill the White tower producing check
-ka= addTower cmb (8,4) B
-testKA = False ~=? checkmate ka B
+killableAggressor= addTower checkmateBlack (8,4) B
+testCheckMateKingCanRemoveAggressor = False ~=? checkmate killableAggressor B
 
--- take cmb from above
+-- take checkmateBlack from above
 -- A tower can move in to safe the king 
-sgp = addTower cmb (2,7) B
-testSGP = False ~=? checkmate sgp B
+scapegoatPiece = addTower checkmateBlack (2,7) B
+testCheckMateScapegoatOther = False ~=? checkmate scapegoatPiece B
 
 checkLimitedMoveTests = TestList[
-    TestLabel "Not into Check Horizontal" testNICH
-    ,TestLabel "Not into Check Vertical" testNICV
-    ,TestLabel "Strike Aggressor" testSA
+    TestLabel "Cannot move King into Check Horizontal" testHorizontalCheckBlockedMoves
+    ,TestLabel "Cannot move King into Check Vertical" testVerticalCheckBlockedMoves
+    ,TestLabel "King can strike Aggressor" testAttackMovesStillWork
+    ,TestLabel "Aggressor is Covered" testAggressorIsCovered
+    ,TestLabel "Aggressor is Covered and Checkmates" testCoveredAggressorCheckmates
     ]
 
-nich = addTower safeKings (8,5) W
-testNICH = 2 ~=? length (validMoves nich B) 
-nicv = addTower safeKings (6,8) W
-testNICV = 2 ~=? length (validMoves nicv B) 
+hoizontalCheckBlocked = addTower safeKings (8,5) W
+testHorizontalCheckBlockedMoves = 2 ~=? length (validMoves hoizontalCheckBlocked B) 
+verticalCheckBlocked = addTower safeKings (6,8) W
+testVerticalCheckBlockedMoves = 2 ~=? length (validMoves verticalCheckBlocked B) 
 
-sa = (addTower safeKings (8,7) W)
+strikableAggressor = (addTower safeKings (8,7) W)
 -- The King can either move away or strike the tower, however i cannot move diagonally as it would mean another check
-testSA = 1 ~=? length (validMoves sa B) 
+testAttackMovesStillWork = 1 ~=? length (validMoves strikableAggressor B)
+
+-- A Queen Guards the Agressing Tower
+coveredAggressor = (addQueen strikableAggressor (7,6) W)
+-- The King cannot move onto the Tower
+testAggressorIsCovered = 0 ~=? length (validMoves coveredAggressor B)
+-- That means he is in checkmate
+testCoveredAggressorCheckmates = True ~=? checkmate coveredAggressor B
